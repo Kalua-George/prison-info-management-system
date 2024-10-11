d61789ea9bc2b59a0748dba054bad0a2875d2d79
Kalua-george <georgekalua605@gmail.com>
2024-10-11 12:27:22 +0200

    these are the added files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # prison-info-management-system
 the final project of web development.. the system is built for assessment purposes and it is a group project.
+  the pages were done primarily by @mphatsoMnyenyembe ,@ireenIsha and @georgekalua
+
+
---
+++ inmate management/js/management.js
@@ -0,0 +1,93 @@
+//holds the 
+const addInmate = document.getElementById('addinmate');
+const demographics = document.getElementById('demographics');
+const transfers = document.getElementById('transfers');
+const viewinmate = document.getElementById('viewinmate');
+
+// Gets the button IDs
+const addButton = document.getElementById('addButton');
+const demoButton = document.getElementById('demoButton');
+const transButton = document.getElementById('transButton');
+const viewButton = document.getElementById('viewButton');
+
+
+const mchart = document.getElementById('maleChart');
+const fchart = document.getElementById('femaleChart');  
+
+addInmate.style.display = 'inherit';
+demographics.style.display = 'none';
+transfers.style.display = 'none';
+viewinmate.style.display = 'none';
+
+//event listeners
+addButton.addEventListener('click', () => {
+    addInmate.style.display = 'inherit';
+    demographics.style.display = 'none';
+    transfers.style.display = 'none';
+    viewinmate.style.display = 'none';
+});
+
+demoButton.addEventListener('click', () => {
+    addInmate.style.display = 'none';
+    demographics.style.display = 'inherit';
+    transfers.style.display = 'none';
+    viewinmate.style.display = 'none';
+});
+
+transButton.addEventListener('click', () => {
+    addInmate.style.display = 'none';
+    demographics.style.display = 'none';
+    transfers.style.display = 'inherit';
+    viewinmate.style.display = 'none';
+});
+
+viewButton.addEventListener('click', () => {
+    addInmate.style.display = 'none';
+    demographics.style.display = 'none';
+    transfers.style.display = 'none';
+    viewinmate.style.display = 'inherit';
+});
+
+
+new Chart(mchart, {
+    type: 'bar',
+    data: {
+        labels: ['15-30', '31-45', '46-60', '61-75', '76-90'],
+        datasets: [{
+            label: 'Male Age Distribution',
+            data: [20, 45, 10, 10, 5],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+new Chart(fchart, {
+    type: 'bar',
+    data: {
+        labels: ['15-30', '31-45', '46-60', '61-75', '76-90'],
+        datasets: [{
+            label: 'Female Age Distribution',
+            data: [20, 45, 32, 3, 0],
+            borderWidth: 1,
+            backgroundColor:'rgb(255,100,50)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+
+
